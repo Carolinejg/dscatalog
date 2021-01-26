806dcc83ed88a2683ff33b1f75210683843efc71
Caroline Souza <loh_jg@hotmail.com>
2021-01-26 20:58:27 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,8 +15,8 @@ public class CategoryService {
 	@Autowired //vai torna o repository uma dependecia válida
 	private CategoryRepository repository;//depsencia com o categoryrepository
 	
-	
-	public List<Category>findAll(){
+	@Transactional
+	public List<Category>findAll(){//garante que qualquer operação realizada neste método está dentro de uma transação
 		return repository.findAll();
 	}
 
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_category  (name) VALUES ('eletronicos');
+INSERT INTO tb_category  (name) VALUES ('camida');
+INSERT INTO tb_category  (name) VALUES ('roupa');
+INSERT INTO tb_category  (name) VALUES ('livros');
+INSERT INTO tb_category  (name) VALUES ('cds');
+INSERT INTO tb_category  (name) VALUES ('dvds');
