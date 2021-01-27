661695123a5366061a1ba980a161d0a0d3cce2b7
Caroline Souza <loh_jg@hotmail.com>
2021-01-26 21:53:24 -0300

    Add DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 //resources: Pacote que implementa o controlador REST
@@ -24,9 +24,9 @@ public class CategoryResource {
 	//o tipo RespondeEntity, tipo do frame que emcapsula uma resposta HTTP.
 	//para configurar que o método vai ser um end point do recurso Category, tem que colocar a anotação a seguir: 
 	@GetMapping
-	public ResponseEntity<List <Category>>findAll()
+	public ResponseEntity<List <CategoryDTO>>findAll()
 	{
-		List<Category> list= service.findAll();
+		List<CategoryDTO> list= service.findAll();
 		return ResponseEntity.ok(list);
 	}
 	
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -15,9 +17,14 @@ public class CategoryService {
 	@Autowired //vai torna o repository uma dependecia válida
 	private CategoryRepository repository;//depsencia com o categoryrepository
 	
-	@Transactional
-	public List<Category>findAll(){//garante que qualquer operação realizada neste método está dentro de uma transação
-		return repository.findAll();
+	@Transactional(readOnly=true)
+	public List<CategoryDTO>findAll() {//garante que qualquer operação realizada neste método está dentro de uma transação
+		List <Category> list = repository.findAll();
+		//para cada elemento da lista de category, tranformo para DTO. 
+		//o collect tranforma os elementos de strem para lista de volta
+		List <CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDTO;
+		
 	}
 
 }
